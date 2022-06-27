11349178f5fc5b05f2c9125a0fdc40812d17580d
Stella <stellamutacho@gmail.com>
2022-06-28 02:20:45 +0300

    Wrote python scripts for EDA

---
+++ scripts/cleaning.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class Cleaner():
+    '''
+    This class contains functions to clean data in a pandas dataframe.
+    '''
+    def __init__(self) -> None:
+        pass
+
+    # def is_categorical(self, df) -> :
+    #     for col in df:
+    #         if 
+
